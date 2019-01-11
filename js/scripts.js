79b3eee71bcca872d901fc9dd6f28f8886adabbc
nyirahabimana dative <nyiradative052@gmail.com>
2019-01-11 11:02:10 +0200

    pass it an object

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,17 @@ function Player(userName) {
     this.randNumber = 0;
     this.player = player;
   };
+  Turn.prototype.diceRoller = function(player1, player2) {
+    var randNumber = Math.floor(Math.random() * 6) + 1;
+    this.total += randNumber;
+  
+    if (randNumber == 1) {
+        this.total = 0;
+        this.endTurn(player1, player2);
+        return randNumber;
+    } else {
+        this.randNumber += randNumber;
+        return randNumber;
+    };
+  };
+  
