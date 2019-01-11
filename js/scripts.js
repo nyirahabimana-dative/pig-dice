3d0748946390e244662af0cd3c255575d8216bbd
nyirahabimana dative <nyiradative052@gmail.com>
2019-01-11 10:58:02 +0200

    pass it  Player object

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,8 @@ function Player(userName) {
     this.userName = userName;
     this.score = 0;
  };
+ function Turn(player) {
+    this.total = 0;
+    this.randNumber = 0;
+    this.player = player;
+  };
